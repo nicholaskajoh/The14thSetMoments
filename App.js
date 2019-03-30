131f18599b3c7866c0b1138e61d7023ac41dbaab
Nicholas Kajoh <kajohterna@gmail.com>
2019-03-30 13:59:17 +0100

    Add better error handling

--- App.js
+++ App.js
@@ -5,15 +5,24 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get('/ig-posts', async (req, res) => {
-  // get 15 most recent IG posts with the hashtag #The14thSetMoments 
-  const hashtag = 'The14thSetMoments';
-  const hashtagRes = await fetch(`https://www.instagram.com/explore/tags/${hashtag}/?__a=1`);
-  const hashtagJson = await hashtagRes.json();
-  res.status(200).json({
-    message: 'Posts retrieved successfully.',
-    data: hashtagJson.graphql.hashtag.edge_hashtag_to_media.edges.slice(0, 15),
-  });
+app.get('/ig-posts', async (req, res, next) => {
+  // get 15 most recent IG posts with the hashtag #The14thSetMoments
+  try {
+    const hashtag = 'The14thSetMoments';
+    const hashtagRes = await fetch(`https://www.instagram.com/explore/tags/${hashtag}/?__a=1`);
+    if (hashtagRes.status == 404) {
+      return res.status(404).json({
+        message: 'Hashtag not found.', data: [],
+      });
+    }
+    const hashtagJson = await hashtagRes.json();
+    return res.status(200).json({
+      message: 'Posts retrieved successfully.',
+      data: hashtagJson.graphql.hashtag.edge_hashtag_to_media.edges.slice(0, 15),
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.listen(port, () => {
