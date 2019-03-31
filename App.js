9a8e5bc260dd2fe8e20156a5e3f0af754e436e64
Nicholas Kajoh <kajohterna@gmail.com>
2019-03-31 13:50:26 +0100

    Add helmet to fix ERR_SSL_PROTOCOL_ERROR on chrome

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const helmet = require('helmet');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(helmet.hsts({ maxAge: 0 }));
 
 app.use(express.static('public'));
 
